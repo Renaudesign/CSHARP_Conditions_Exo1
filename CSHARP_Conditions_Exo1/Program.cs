eb32386bc3e0a31e486713266f25875a06dacc19
renaudesign <renaud.sourmail@gmail.com>
2021-02-17 11:32:39 +0100

    correction. simplicitation du renay == true en rainy. Ajout de blocs

--- CSHARP_Conditions_Exo1/Program.cs
+++ CSHARP_Conditions_Exo1/Program.cs
@@ -8,11 +8,13 @@ namespace CSHARP_Conditions_Exo1
         {
             bool rainy = true;
 
-            if(rainy==true)
-            Console.WriteLine("Bonjour, attention aux pluies radioactives aujourd'hui. Pensez à mettre votre combinaison étanche intégrale Vault-Tec. Bonne journée !");
-
+            if (rainy)
+            {
+                Console.WriteLine("Bonjour, attention aux pluies radioactives aujourd'hui. Pensez à mettre votre combinaison étanche intégrale Vault-Tec. Bonne journée !");}
             else
-            Console.WriteLine("Bonjour, aujourd'hui il n'y pas de pluies radioactives, bonne journée !");
+            {
+                Console.WriteLine("Bonjour, aujourd'hui il n'y pas de pluies radioactives, bonne journée !");}
+            
         }
     }
 }
